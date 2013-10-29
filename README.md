144717e3deb32930a264ba466935340dbdf47e99
Hugh Kennedy <hughskennedy@gmail.com>
2013-10-29 15:10:01 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# loading-bar [![experimental](http://hughsk.github.io/stability-badges/dist/experimental.svg)](http://github.com/hughsk/stability-badges) #
+
+![loading bar](http://i.imgur.com/ITc3qFX.png)
+
+A quick-and-dirty loading bar for use in your larger demos.
+
+## Usage ##
+
+[![loading-bar](https://nodei.co/npm/loading-bar.png?mini=true)](https://nodei.co/npm/loading-bar)
+
+## `require('loading-bar')([fill, width, height, border])` ##
+
+Creates a new loading bar element, ready to be attached to the DOM. All
+arguments are optional:
+
+* `fill`: the color of the loading bar, as a CSS string. Defaults to `#fff`
+* `width`: the width of the loading bar in pixels. Defaults to `500`
+* `height`: the height of the loading bar in pixels. Defaults to `30`
+* `border`: the size of the inner and outer border in pixels. Defaults to `4`.
+
+## `bar.append([parent])` ##
+
+Appends the loading bar to `parent`, which defaults to `document.body`.
+
+## `bar.update(t)` ##
+
+Pass a value between 0 and 1 to this function to update the progress of the
+loading bar.
+
+## `bar.remove()` ##
+
+Removes the bar from the DOM.
+
+## `bar.fadeout([duration])` ##
+
+Fades the loading bar out over `duration` milliseconds. Defaults to 1 second.
+
+## License ##
+
+MIT. See [LICENSE.md](http://github.com/hughsk/loading-bar/blob/master/LICENSE.md) for details.
---
+++ index.js
@@ -0,0 +1,73 @@
+module.exports = bar
+
+function bar(fill, width, height, margin) {
+  if (!(this instanceof bar)) return new bar(fill)
+  fill = fill || '#FFF'
+  width = this.width = +(parseFloat(width) || 500)
+  height = this.height = +(parseFloat(height) || 30)
+  margin = this.margin = +(parseFloat(margin) || 4)
+
+  var wrap = this.element = document.createElement('div')
+  wrap.style.border = margin + 'px solid ' + fill
+  wrap.style.position = 'absolute'
+  wrap.style.zIndex = 99999
+  wrap.style.top = '50%'
+  wrap.style.left = '50%'
+  wrap.style.height = height + 'px'
+  wrap.style.width = width + 'px'
+  wrap.style.marginTop = (-height/2)+'px'
+  wrap.style.marginLeft = (-width/2)+'px'
+
+  var inner = this.inner = document.createElement('div')
+  inner.style.backgroundColor = fill
+  inner.style.margin = margin + 'px'
+  inner.style.height = (height - margin * 2) + 'px'
+  inner.style.width = '0'
+  inner.style.webkitTransition =
+  inner.style.mozTransition =
+  inner.style.msTransition =
+  inner.style.oTransition =
+  inner.style.transition = 'width 1.0s'
+
+  wrap.appendChild(inner)
+}
+
+bar.prototype.update = function(n) {
+  n = +(parseFloat(n) || 0)
+  this.inner.style.width = (n*this.width - this.margin*2) + 'px'
+  return this
+}
+
+bar.prototype.append = function(parent) {
+  parent = parent || document.body
+  parent.appendChild(this.element)
+  return this
+}
+
+bar.prototype.remove = function() {
+  if (this.element.parentNode) {
+    this.element.parentNode.removeChild(this.element)
+  }
+
+  return this
+}
+
+bar.prototype.fadeout = function(duration) {
+  var wrap = this.element
+  var self = this
+
+  duration = +(parseFloat(duration) || 1000) / 1000
+
+  wrap.style.webkitTransition =
+  wrap.style.mozTransition =
+  wrap.style.msTransition =
+  wrap.style.oTransition =
+  wrap.style.transition = 'opacity '+duration+'s'
+
+  setTimeout(function() {
+    wrap.style.opacity = 0
+  }, 100)
+  setTimeout(function() {
+    self.remove()
+  }, duration * 1000)
+}
